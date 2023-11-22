5a9243313203d365d8ba5c4d45c3aa2293b0a6a6
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-22 10:31:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+ let first=prompt("Enter first number")*2;
+ let second=prompt("Enter second number");
+let res=parseInt(first)+parseInt(second);
+ alert("The final result is:"+ res);
